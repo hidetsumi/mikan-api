694622322faddce55aa09d38ae32ce4842bb7088
hidetsumi <hidetsu99@gmail.com>
2026-05-06 00:11:00 -0300

    fix: add controllers to test

--- src/modules/users/application/users.service.spec.ts
+++ src/modules/users/application/users.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateUserService } from './create-user.service';
+import { UsersRepository } from '../domain/repository/user.repository';
 
 describe('UsersService', () => {
   let service: CreateUserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CreateUserService],
+      providers: [
+        CreateUserService,
+        {
+          provide: UsersRepository,
+          useValue: {
+            findByEmail: jest.fn(),
+            create: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<CreateUserService>(CreateUserService);
